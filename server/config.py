728db4ae50ba5e2adf8c3bec137b3d7de72f9780
vitamaxoduol <vitamaxoduo@gmail.com>
2023-10-30 19:40:17 +0300

    redis

--- server/config.py
+++ server/config.py
@@ -17,6 +17,6 @@ class Config:
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_REDIS = redis.from_url('redis://127.0.0.1:6379')
-    SESSION_REDIS = redis.from_url(os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379'))
+    SESSION_REDIS = redis.from_url(os.environ.get('REDIS_URL', 'redis://red-ckvtnp3amefc73bpiip0:6379'))
 
     
